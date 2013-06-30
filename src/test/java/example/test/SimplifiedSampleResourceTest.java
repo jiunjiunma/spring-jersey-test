e4a192732ae17264b3e461a6433e370cd2674753
Jiunjiun Ma <jiunjiunma@gmail.com>
2013-06-30 10:20:47 -0700

    further simplification
    remove @Configuration on derived test class and add a simplified version

--- src/test/java/example/test/SimplifiedSampleResourceTest.java
+++ src/test/java/example/test/SimplifiedSampleResourceTest.java
@@ -1,7 +1,6 @@
 package example.test;
 
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 import org.springframework.context.annotation.Bean;
@@ -13,16 +12,19 @@ import example.rest.SampleResource;
 import example.service.SampleService;
 
 /**
- * Date: 6/11/13
- * Time: 11:08 AM
+ * Same test as SampleResourceTest, further simplified.
+ *
+ * Date: 6/30/13
+ * Time: 10:16 AM
  */
-public class SampleResourceTest extends SpringContextAwareJerseyTest {
-    private SampleService mockSampleService;
-
-    // create mock object for our test
+public class SimplifiedSampleResourceTest extends SpringContextAwareJerseyTest {
     @Bean
     static public SampleService sampleService() {
-        return Mockito.mock(SampleService.class);
+        SampleService mockSampleService =  Mockito.mock(SampleService.class);
+        // see how the mock object hijack the sample service, now id 3 is valid
+        Sample sample3 = new Sample(3, "sample3");
+        Mockito.when(mockSampleService.getSample(3)).thenReturn(sample3);
+        return mockSampleService;
     }
 
     /**
@@ -35,28 +37,11 @@ public class SampleResourceTest extends SpringContextAwareJerseyTest {
         return new SampleResource();
     }
 
-    // get the mock objects from the internal servlet context, because
-    // the app context may get recreated for each test so we have to set
-    // it before each run
-    @Before
-    public void setupMocks() {
-         mockSampleService = getContext().getBean(SampleService.class);
-    }
-
-    @Test
-    public void testMock() {
-        Assert.assertNotNull(mockSampleService);
-    }
 
     @Test
     public void testGetSample() {
-        // see how the mock object hijack the sample service, now id 3 is valid
-        Sample sample3 = new Sample(3, "sample3");
-        Mockito.when(mockSampleService.getSample(3)).thenReturn(sample3);
-
         expect().statusCode(200).get(SERVLET_PATH + "/sample/3");
         String jsonStr = get(SERVLET_PATH + "/sample/3").asString();
         Assert.assertNotNull(jsonStr);
     }
-
 }
